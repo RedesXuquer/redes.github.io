1d194fac724f53397f66829cb7a0420f72eb126e
RedesXuquer <laleza@axuquer.es>
2023-09-23 12:01:47 +0000

    commit inicial

--- _site/assets/js/data/swcache.js
+++ _site/assets/js/data/swcache.js
@@ -1,35 +1,35 @@
 const resource = [
   /* --- CSS --- */
-  '/redes.github.io/_site/assets/css/jekyll-theme-chirpy.css',
+  '/assets/css/jekyll-theme-chirpy.css',
 
   /* --- PWA --- */
-  '/redes.github.io/_site/app.js',
-  '/redes.github.io/_site/sw.js',
+  '/app.js',
+  '/sw.js',
 
   /* --- HTML --- */
-  '/redes.github.io/_site/index.html',
-  '/redes.github.io/_site/404.html',
+  '/index.html',
+  '/404.html',
 
   
-    '/redes.github.io/_site/categories/',
+    '/categories/',
   
-    '/redes.github.io/_site/tags/',
+    '/tags/',
   
-    '/redes.github.io/_site/archives/',
+    '/archives/',
   
-    '/redes.github.io/_site/about/',
+    '/about/',
   
 
   /* --- Favicons & compressed JS --- */
   
   
-    '/redes.github.io/_site/assets/img/favicons/android-chrome-192x192.png',
-    '/redes.github.io/_site/assets/img/favicons/android-chrome-512x512.png',
-    '/redes.github.io/_site/assets/img/favicons/apple-touch-icon.png',
-    '/redes.github.io/_site/assets/img/favicons/favicon-16x16.png',
-    '/redes.github.io/_site/assets/img/favicons/favicon-32x32.png',
-    '/redes.github.io/_site/assets/img/favicons/favicon.ico',
-    '/redes.github.io/_site/assets/img/favicons/mstile-150x150.png'
+    '/assets/img/favicons/android-chrome-192x192.png',
+    '/assets/img/favicons/android-chrome-512x512.png',
+    '/assets/img/favicons/apple-touch-icon.png',
+    '/assets/img/favicons/favicon-16x16.png',
+    '/assets/img/favicons/favicon-32x32.png',
+    '/assets/img/favicons/favicon.ico',
+    '/assets/img/favicons/mstile-150x150.png'
 ];
 
 /* The request url with below domain will be cached */
