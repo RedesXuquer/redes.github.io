9aa1fb3769db15e26fa105fd2cd962f976f2d942
RedesXuquer <laleza@axuquer.es>
2023-10-30 22:11:13 +0000

    commit inicial

--- _site/sw.js
+++ _site/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/redes.github.io/_site/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20231030.231054';
+const cacheName = 'chirpy-20231030.231113';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
